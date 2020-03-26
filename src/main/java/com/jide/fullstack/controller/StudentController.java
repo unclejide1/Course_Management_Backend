d98068085c163c154d693efcb9d8e6bcf5563538
Jideofor <njusticej@gmail.com>
2020-03-26 12:36:35 +0100

    feat: add student controller

---
+++ src/main/java/com/jide/fullstack/controller/StudentController.java
@@ -0,0 +1,33 @@
+package com.jide.fullstack.controller;
+
+import com.jide.fullstack.model.Gender;
+import com.jide.fullstack.model.Student;
+import com.jide.fullstack.services.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("students")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping
+    public List<Student> getAllStudents(){
+        return studentService.getAllStudents();
+    }
+
+    @PostMapping
+    public void addNewStudent( @RequestBody Student student){
+        studentService.addNewStudent(student);
+    }
+
+}
