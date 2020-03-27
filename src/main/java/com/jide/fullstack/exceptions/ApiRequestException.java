080417e3acd623338bd88dbe48e55b6073ab6706
Jideofor <njusticej@gmail.com>
2020-03-27 02:15:55 +0100

    feat: add Exception Handling

---
+++ src/main/java/com/jide/fullstack/exceptions/ApiRequestException.java
@@ -0,0 +1,12 @@
+package com.jide.fullstack.exceptions;
+
+public class ApiRequestException extends RuntimeException {
+
+    public ApiRequestException(String message) {
+        super(message);
+    }
+
+    public ApiRequestException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
